90b0d1e961f935133416ba5e39c2da4d33fc4dad
Luis Gonzales <luisgonzacc@gmail.com>
2022-05-11 22:25:00 +0000

    Task 1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/env pup
-# This manifest kills a process named killmenow.
-exec { 'pkill':
-  command => 'pkill killmenow',
-  path    => '/usr/local/bin/:/bin/',
+#Using Puppet, install flask from pip3.
+exec {'install_flask':
+  command  => 'pip3 install Flask==2.1.0',
+  provider => shell,
 }
