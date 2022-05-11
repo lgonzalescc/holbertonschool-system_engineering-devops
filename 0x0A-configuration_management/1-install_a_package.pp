9c64069f1a56b3b439a877eccfed93d9c8108a3a
Luis Gonzales <luisgonzacc@gmail.com>
2022-05-11 21:51:38 +0000

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install a package - Using Puppet, install puppet-lint.
 
 package { 'instalar flask':
-	command => 'pip3 install flask == 2.1.0',
-	provider => shell,
+  command  => 'pip3 install flask == 2.1.0',
+  provider => shell,
 }
